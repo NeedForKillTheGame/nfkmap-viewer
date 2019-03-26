ea74d5c36f65e2cb184a36fcfc3943c9332f394b
HarpyWar <harpywar@gmail.com>
2019-03-26 12:05:29 +0300

    fix autoloader.php

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -38,7 +38,7 @@ class Autoloader
      */
     public static function autoload($class)
     {
-        if (is_file($file = dirname(__FILE__).'/'.str_replace(array('_', "\0"), array('/', ''), $class).'.php')) {
+        if (is_file($file = dirname(__FILE__).'/'.str_replace(array('_', "\0", '\\'), array('/', '', '/'), $class).'.php')) {
             require $file;
         }
     }
